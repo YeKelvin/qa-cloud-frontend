ca51c3495e62aedad287f0736dae540452c80d2a
Kelvin.Ye <testmankelvin@163.com>
2023-06-14 10:50:35 +0800

    🐞 fix: 魔改keep-alive失败

--- build/modify-keepalive.js
+++ build/modify-keepalive.js
@@ -5,9 +5,9 @@ const vue_bundler_file = path.resolve(__dirname, '../node_modules/@vue/runtime-c
 fs.readFile(vue_bundler_file, 'utf8', function (err, data) {
   if (err) console.error(err)
   const orginal_str =
-    "        if ((process.env.NODE_ENV !== 'production') || __VUE_PROD_DEVTOOLS__) {\n            instance.__v_cache = cache;\n        }"
+    '    if (!!(process.env.NODE_ENV !== "production") || __VUE_PROD_DEVTOOLS__) {\n      instance.__v_cache = cache;\n    }'
   const target_str =
-    "        //if ((process.env.NODE_ENV !== 'production') || __VUE_PROD_DEVTOOLS__) {\n            instance.__v_cache = cache;\n        //}"
+    '    // if (!!(process.env.NODE_ENV !== "production") || __VUE_PROD_DEVTOOLS__) {\n      instance.__v_cache = cache;\n    // }'
   const result = data.replace(orginal_str, target_str)
   fs.writeFile(vue_bundler_file, result, 'utf8', function (err) {
     if (err) return console.error(err)
